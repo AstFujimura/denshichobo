cbc9dada642f19393c08bf937a83b0ccaf40e1fd
Fujimura <fujimura@ast-sys.co.jp>
2023-08-29 16:19:33 +0900

    js

--- resources/views/layouts/admintemplate.blade.php
+++ resources/views/layouts/admintemplate.blade.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/adminstyle.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/regist.css') }}">
-    <script src="{{asset('jquery/jquery-3.7.0.min')}}"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <noscript>
         <meta http-equiv="refresh" content="0;URL=/jserror" />
