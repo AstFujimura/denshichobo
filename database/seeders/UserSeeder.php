d80c422aeb84f2a1ddd60dfb3f25c84656b345f0
Fujimura <fujimura@ast-sys.co.jp>
2023-08-29 16:30:46 +0900

    create2

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,7 +19,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+        DB::table('users')->insert([
             [
                 'name' => 'astec',
                 'email' => 'astec@ast-sys.co.jp',
