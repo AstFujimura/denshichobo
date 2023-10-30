a3aee015f9572fbf0db25d7621113333e66d7a6c
Fujimura <fujimura@ast-sys.co.jp>
2023-10-30 13:11:53 +0900

    ver2.7.3

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -33,7 +33,7 @@
         <input type="hidden" id="prefix" value="{{config('prefix.prefix')}}">
     </div>
     
-    <span class="version">ver.2.7.2</span>
+    <span class="version">ver.2.7.3</span>
 
 </header>
 <div class="menu001">
