6230dfca0fce0724f16632ef3b542b228553fc9c
Fujimura <fujimura@ast-sys.co.jp>
2023-10-30 14:06:21 +0900

    adminにprefix

--- resources/views/layouts/admintemplate.blade.php
+++ resources/views/layouts/admintemplate.blade.php
@@ -28,7 +28,7 @@
             管理画面
         </a>
         <input type="hidden" id="server" value="{{config('prefix.server')}}">
-        <input type="hidden" id="prefix" value="{{config('prefix.prefix')}}">
+        <input type="hidden" id="prefix" value="{{$prefix}}">
     </div>
     <span class="version">ver.2.7.3</span>
 </header>
