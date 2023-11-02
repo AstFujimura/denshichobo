5b72e168cc07314466765343a9b3145d6d5ba464
Fujimura <fujimura@ast-sys.co.jp>
2023-11-02 10:11:33 +0900

    274

--- resources/views/layouts/admintemplate.blade.php
+++ resources/views/layouts/admintemplate.blade.php
@@ -30,7 +30,7 @@
         <input type="hidden" id="server" value="{{config('prefix.server')}}">
         <input type="hidden" id="prefix" value="{{$prefix}}">
     </div>
-    <span class="version">ver.2.7.3</span>
+    <span class="version">ver.2.7.4</span>
 </header>
 <div class="menu001">
 
