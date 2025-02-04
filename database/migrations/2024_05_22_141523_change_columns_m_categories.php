ac6fe88e8a2b93db83caf196541d4881b38ee01a
fujimura-astec <fujimura@ast-sys.co.jp>
2025-02-04 16:34:28 +0900

    nullable

--- database/migrations/2024_05_22_141523_change_columns_m_categories.php
+++ database/migrations/2024_05_22_141523_change_columns_m_categories.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::table('m_categories', function (Blueprint $table) {
-            $table->decimal('縦', 10, 5)->nullable()->change();
-            $table->decimal('横', 10, 5)->nullable()->change();
+            $table->decimal('縦', 10, 5)->nullable();
+            $table->decimal('横', 10, 5)->nullable();
         });
     }
 
@@ -27,8 +27,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('m_categories', function (Blueprint $table) {
-            $table->dropColumn('縦')->nullable(false)->change();
-            $table->dropColumn('横')->nullable(false)->change();
+            $table->decimal('縦')->nullable(false)->change();
+            $table->decimal('横')->nullable(false)->change();
         });
     }
 };
