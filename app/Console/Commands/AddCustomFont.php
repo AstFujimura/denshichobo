4fa643c79918d07869baf644c6a56cef5a49d089
fujimura-astec <fujimura@ast-sys.co.jp>
2025-03-03 17:22:50 +0900

    add

---
+++ app/Console/Commands/AddCustomFont.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use TCPDF_FONTS;
+
+class AddCustomFont extends Command
+{
+    protected $signature = 'tcpdf:add-font';
+    protected $description = 'Add a custom font to TCPDF';
+
+    public function handle()
+    {
+        // Laravelの `public/font/Noto.ttf` を取得
+        $fontFile = public_path('font/Noto.ttf');
+
+        if (!file_exists($fontFile)) {
+            $this->error("Font file not found: $fontFile");
+            return;
+        }
+
+        // TCPDF フォントフォルダに登録
+        $fontName = TCPDF_FONTS::addTTFfont($fontFile, 'TrueTypeUnicode', '', 96);
+
+        if ($fontName) {
+            $this->info("Font successfully added: $fontName");
+        } else {
+            $this->error("Failed to add font.");
+        }
+    }
+}
