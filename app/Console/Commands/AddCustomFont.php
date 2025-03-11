2aaa31881d6a2fd5fbed47f28febaaaf71828bd5
fujimura-astec <fujimura@ast-sys.co.jp>
2025-03-11 16:08:19 +0900

    add

--- app/Console/Commands/AddCustomFont.php
+++ app/Console/Commands/AddCustomFont.php
@@ -15,6 +15,13 @@ class AddCustomFont extends Command
         // Laravelの `public/font/Noto.ttf` を取得
         $s3FontUrl  = 'https://astdocs-public.s3.ap-northeast-1.amazonaws.com/font/Noto.ttf';
         $localFontPath = storage_path('fonts/Noto.ttf');
+
+        // **ディレクトリが存在しなければ作成**
+        $fontDir = storage_path('fonts');
+        if (!file_exists($fontDir)) {
+            mkdir($fontDir, 0775, true);
+        }
+
         // フォントをローカルに保存
         if (!file_exists($localFontPath)) {
             $this->info("Downloading font from S3...");
